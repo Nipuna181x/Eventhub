bddd781d702d74cb2bc9cb58c94db77f9074e7a2
Nipuna181x <nipunadhananjaya181@gmail.com>
2025-11-04 00:47:24 +0530

    admin dashbord:done

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Event;
 
 class AdminController extends Controller
 {
     public function dashboard(){
-        return view('admin.dashboard');
+        $recentEvents = Event::latest()->take(5)->get();
+        return view('admin.dashboard', compact('recentEvents'));
     }
 }
